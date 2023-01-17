3fb287812797c38d8e3f3f55560ef82e7d38cd1b
bravoarthur <arthur.bravo08@gmail.com>
2023-01-17 23:50:58 +1100

    Folder public solved

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,8 @@
 import {Request, Response} from 'express'
 
 export const home = (req: Request, res: Response) => {
-    res.send('Home no controller')
+    res.render('pages/page')
+   
 }
 
 export const dogs = (req: Request, res: Response) => {
